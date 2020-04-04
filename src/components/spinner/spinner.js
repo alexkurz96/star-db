3dfeb4803a56faf1f1e8cb405292ee86004cb62c
alexkurz96 <akex-kurz@mail.ru>
2020-04-04 18:34:42 +0300

    first commit

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+	return (
+		<div className="loadingio-spinner-double-ring-540tkfbzne5">
+			<div className="ldio-6s8kivajbci">
+				<div></div>
+				<div></div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Spinner;
